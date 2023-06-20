c60b0a523d82fc941904bcaeb801cfc5a280314d
Charaf-Z <charafomarzitouni@gmail.com>
2023-06-20 10:42:04 -0700

    uploading the 11-print_to_98.c file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: starter number to print
+ */
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		printf("%d, ", n);
+		if (n < 98)
+		{
+			n++;
+		}
+		else
+		{
+			n--;
+		}
+	}
+	printf("%d\n", n);
+}
