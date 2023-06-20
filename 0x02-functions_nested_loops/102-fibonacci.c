e13c912e5cf179c9ca8f308b828dea954eff1254
Charaf-Z <charafomarzitouni@gmail.com>
2023-06-20 13:46:06 -0700

    update the 102-fibonacci.c file

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,16 +8,16 @@ int main(void)
 	int i;
 	unsigned long first = 0, second = 1, sum;
 
-	for (i = 0; i <= 50; i++)
+	for (i = 1; i <= 50; i++)
 	{
 		sum = first + second;
 		printf("%lu", sum);
-		first = second;
-		second = sum;
 		if (i != 50)
 		{
 			printf(", ");
 		}
+		first = second;
+		second = sum;
 	}
 	return (0);
 }
