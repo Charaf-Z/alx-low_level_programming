c2186552d2b3c7542a7c7c1900016fa5c8521e02
Charaf-Z <charafomarzitouni@gmail.com>
2023-06-20 17:27:08 -0700

    uploading the 104-fibonacci.c file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,26 +1,54 @@
 #include<stdio.h>
 /**
+ * nbrLength - returns the number of character of number
+ * @nbr: the number to get its length
+ * Return: number of digits
+ */
+int nbrLength(int nbr)
+{
+	int length = 0;
+
+	if (!nbr)
+		return (1);
+	while (nbr)
+	{
+		nbr = nbr / 10;
+		length++;
+	}
+	return (length);
+}
+
+/**
  * main - Entry point
- * Return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int count = 2, limit = 100;
-	unsigned long first = 1, second = 2, next;
+	int count, initial_zero;
+	unsigned long first = 1, second = 2, next, max = 100000000;
+	unsigned long first_part = 0, second_part = 0, next_part = 0;
 
-	printf("%lu, %lu, ", first, second);
-	while (count < limit)
+	for (count = 1; count <= 98; count++)
 	{
-		next = first + second;
-		printf("%lu", next);
-		if (count < (limit - 1))
+		if (first_part > 0)
+			printf("%lu", first_part);
+		initial_zero = nbrLength(max) - 1 - nbrLength(first);
+		while (first_part > 0 && initial_zero > 0)
 		{
-			printf(", ");
+			printf("%d", 0);
+			initial_zero--;
 		}
+		printf("%lu", first);
+		next = (first + second) % max;
+		next_part = first_part + second_part + (first + second) / max;
 		first = second;
+		first_part = second_part;
 		second = next;
-		count++;
+		second_part = next_part;
+		if (count != 98)
+			printf(", ");
+		else
+			printf("\n");
 	}
-	printf("\n");
 	return (0);
 }
