e027dd749103ad5974147bf65483ba489e3ecb74
Charaf-Z <charafomarzitouni@gmail.com>
2023-06-22 11:29:19 -0700

    uploading the 3-print_numbers.c file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	int nb;
+
+	for (nb = 48; nb <= 57; nb++)
+		_putchar(nb);
+	_putchar('\n');
+}
