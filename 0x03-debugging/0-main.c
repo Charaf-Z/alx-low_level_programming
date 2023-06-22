fa0cdad93363d36d40e7608725c40afb6ef95fa2
Charaf-Z <charafomarzitouni@gmail.com>
2023-06-22 09:43:37 -0700

    uploading the 0-main.c file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - Test function for positive or negative
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
