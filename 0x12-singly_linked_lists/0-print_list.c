110e9fa9926af9fa7782280fc8cf08cb91445e3f
Charaf-Z <charafomarzitouni@gmail.com>
2023-07-27 06:47:01 -0700

    update 0-print_list file

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -27,7 +27,7 @@ size_t print_list(const list_t *h)
 	size_t i = 0;
 
 	while (h)
-		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)"),
+		printf("[%u] %s\n", !h->str ? 0 : h->len, h->str ? h->str : "(nil)"),
 			h = h->next, i++;
 	return (i);
 }
