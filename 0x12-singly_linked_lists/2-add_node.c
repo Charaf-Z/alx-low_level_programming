32c567893113ff8646870e96991fb1f9ca52b14b
Charaf-Z <charafomarzitouni@gmail.com>
2023-07-27 07:14:21 -0700

    update 2-add_node file

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,7 +8,7 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_head = malloc(sizeof(ist_t));
+	list_t *new_head = malloc(sizeof(list_t));
 
 	if (!head || !new_head)
 		return (NULL);
@@ -21,6 +21,7 @@ list_t *add_node(list_t **head, const char *str)
 			return (NULL);
 		}
 		new_head->len == _strlen(new_head->str);
+		new_head->next = NULL;
 	}
 	new_head->next = *head;
 	*head = new_head;
